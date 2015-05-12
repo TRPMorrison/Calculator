901259bc08b9b36222571ef6710f0f74e0566aef
Trini Morrison <TRPMorrison@gmail.com>
2015-05-12 13:56:41 -0400

    DRAFT OF JS/LINKED FILES IN HTML

---
+++ main.js
@@ -0,0 +1,38 @@
+// 1. Use the HTML provided in index.html and set up a new project
+
+// 2. Create a main.js file and link it to your index.html file
+
+// 3.  Using the DOM API and the elements on the page, set up the following:
+
+// When I click the "calculate" button, the values in the first box should be added, subtracted, multiplied or divided by the value in the second box.
+
+// You'll need to convert the values to a number (think Number(value)) in order to process them.
+// Your output should be injected into the answer element
+
+//4. Add some styles to this project and make it something pretty to look at.
+
+function calculate(answer) {
+  return
+}
+
+
+var number1 = value1("".getAttribute('num_one'));
+
+var number2 = value2("".getAttribute('num_two'));
+
+function calculate(value) {
+  var calculateBtn = event.target;
+  var calculateBtn = document.querySelector("submit");
+
+}
+var calculateBtn = event.target;
+var calculateBtn = document.querySelector("submit");
+
+num_one.addEventListener("submit", calculate);
+num_two.addEventListener("submit", calculate);
+
+function response() {
+  alert(Answer);
+}
+
+calculateBtn.addEventListener("click", response);
